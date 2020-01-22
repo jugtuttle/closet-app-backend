63c5525b1a06a7736f0c35c587df565db6b8efb2
jugtuttle <jugtuttle@gmail.com>
2020-01-21 17:41:14 -0800

    added main methods to outfit controller

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -3,4 +3,43 @@ class OutfitsController < ApplicationController
         @outfits = Outfit.all 
         render :json => @outfits
     end 
+
+    def new 
+        @outfit = Outfit.new
+    end 
+
+    def edit 
+        @outfit = Outfit.find(params[:id])
+    end 
+
+    def create 
+        @outfit = Outfit.new(outfit_params)
+        if @outfit.create
+            render :json => @outfit
+        else 
+            NULL
+            ### needs error handling
+        end  
+    end 
+
+    def update 
+        @outfit = Outfit.find(params[:id])
+        if @outfit.update
+            render :json => @outfit
+        else 
+            NULL 
+            ### needs error handling 
+        end 
+    end 
+
+    def destroy
+        @outfit = Outfit.find(params[:id])
+        @outfit.destroy 
+    end
+
+    private 
+
+    def outfit_params 
+        params.require(:outfit).permit(:name)
+    end
 end
