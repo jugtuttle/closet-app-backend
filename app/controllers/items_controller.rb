f3127eb101941989ab599b6d18301f5a0be4b72e
jugtuttle <jugtuttle@gmail.com>
2020-01-21 17:32:19 -0800

    added new/create/update/delete/item_params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,43 @@ class ItemsController < ApplicationController
     def index 
         @items = Item.all 
         render :json => @items 
+    end
+
+    def new 
+        @item = Item.new
     end 
+
+    def show 
+        @item = Item.find(params[:id])
+    end 
+    
+    def create 
+        @item = Item.new(item_params)
+        if @item.create
+        render :json => @item 
+        else 
+            NULL #### needs error handling 
+        end 
+    end 
+
+    def update 
+        @item = Item.find(params[:id])
+        if @item.update 
+            render :json => @item 
+        else 
+            NULL #### needs error handling 
+        end
+    end 
+
+    def destroy
+        @item = Item.find(params[:id])
+        @item.destroy 
+    end
+
+    private 
+
+    def item_params
+        params.require(:item).permit(:image_url, :name, :times_worn, :favorite, :user_id)
+    end
+
 end
