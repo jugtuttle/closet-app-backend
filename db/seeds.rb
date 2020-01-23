0904418b1d8e1bc3506bb424ccfca4ed068a72b3
jmknopf1007 <jmknopf1007@gmail.com>
2020-01-23 13:52:24 -0800

    messing with seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# User.delete_all
+# User.reset_pk_sequence
+# Item.delete_all
+# Item.reset_pk_sequence
+# Category.delete_all
+# Category.reset_pk_sequence
+# Outfit.delete_all
+# Outfit.reset_pk_sequence
+# ItemCategory.delete_all
+# ItemCategory.reset_pk_sequence
+# OutfitItem.delete_all
+# OutfitItem.reset_pk_sequence
 
-User.create(username: "Josh")
-Item.create(image_url: "https://cdn.shopify.com/s/files/1/2495/5044/products/oca-high-camel-brown-suede-side-view.slideshow1_2800x.jpg?v=1568779201", name: "Brown Shoes", times_worn: 0, user_id: 1, favorite: false)
-Category.create(name: "brown", group: "color")
-Outfit.create(name: "first outfit")
-ItemCategory.create(item_id: 1, category_id: 1)
-OutfitItem.create(item_id: 1, outfit_id: 1) 
 
-<<<<<<< HEAD
-#testing by Amy
 
-#test Jacob K 
-=======
-
-#Items 
-
-# Item.create(image_url: "https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwffe1226c/images/hi-res/84217_BLK.jpg?sw=750&sh=750&sm=fit&sfrm=png", name: "Black Jacket", times_worn: 0, user_id: 1, favorite: false)
+# User.create(username: "Josh")
+# Item.create(image_url: "https://cdn.shopify.com/s/files/1/2495/5044/products/oca-high-camel-brown-suede-side-view.slideshow1_2800x.jpg?v=1568779201", name: "Brown Shoes", times_worn: 0, user_id: 1, favorite: false)
 # Item.create(image_url: "https://img.cdn.mountainwarehouse.com/product/024307/024307_dka_featherweight_down_padded_jacket_men_ss19_2.jpg", name: "Green Jacket", times_worn: 0, user_id: 1, favorite: false)
 # Item.create(image_url: "https://lsco.scene7.com/is/image/lsco/levis/clothing/723340131-alt1-pdp.jpg?$regular_mobile$", name: "Denim Jacket", times_worn: 0, user_id: 1, favorite: false)
-
-
-
-
-# ItemCategory.create(item_id: 1, category_id: 2)
-# ItemCategory.create(item_id: 2, category_id: 2)
-# ItemCategory.create(item_id: 3, category_id: 2)
-
-# ItemCategory.create(item_id: 2, category_id: 3)
-# ItemCategory.create(item_id: 3, category_id: 3)
-
-##categories 
+# Item.create(image_url: "https://www.patagonia.com/dis/dw/image/v2/ABBM_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwffe1226c/images/hi-res/84217_BLK.jpg?sw=750&sh=750&sm=fit&sfrm=png", name: "Black Jacket", times_worn: 0, user_id: 1, favorite: false)
 # Category.create(name: "Shoes", group: "Clothing")
 # Category.create(name: "Tops", group: "Clothing")
 # Category.create(name: "Bottoms", group: "Clothing")
@@ -44,5 +26,26 @@ OutfitItem.create(item_id: 1, outfit_id: 1)
 # Category.create(name: "Bottoms", group: "Clothing")
 # Category.create(name: "Glasses", group: "Accessories")
 # Category.create(name: "Hats", group: "Accessories")
+# Category.create(name: "brown", group: "color")
+# Outfit.create(name: "first outfit")
+# Outfit.create(name: "rainy day")
+# ItemCategory.create(item_id: 1, category_id: 1)
+# ItemCategory.create(item_id: 1, category_id: 2)
+# ItemCategory.create(item_id: 2, category_id: 2)
+# ItemCategory.create(item_id: 3, category_id: 2)
 
->>>>>>> dev
+# ItemCategory.create(item_id: 2, category_id: 3)
+# ItemCategory.create(item_id: 3, category_id: 3)
+# OutfitItem.create(item_id: 1, outfit_id: 1) 
+# OutfitItem.create(item_id: 2, outfit_id: 2) 
+
+
+
+
+
+
+
+
+
+
+
