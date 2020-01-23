ba3b65aa45fd8368f338959d592a777a58f5ccd8
jmknopf1007 <jmknopf1007@gmail.com>
2020-01-23 09:40:52 -0800

    first backend commit

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :image_url, :name, :times_worn, :favorite
+  attributes :id, :image_url, :name, :times_worn, :favorite, :outfits
   has_one :user
 end
--- app/serializers/outfit_serializer.rb
+++ app/serializers/outfit_serializer.rb
@@ -1,3 +1,3 @@
 class OutfitSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :items 
 end
