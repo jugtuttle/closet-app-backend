c1c976e63f42cd1d9da20470bb972a399ca6eae6
jugtuttle <jugtuttle@gmail.com>
2020-01-24 11:20:02 -0800

    create outfit working

--- app/controllers/outfit_items_controller.rb
+++ app/controllers/outfit_items_controller.rb
@@ -4,6 +4,12 @@ class OutfitItemsController < ApplicationController
         render :json => @outfit_items
     end 
 
+    def create
+        # byebug
+        @OutfitItem = OutfitItem.find_or_create_by(item_id: params[:item_id], outfit_id: params[:outfit_id])
+        render :json => @OutfitItem
+    end
+
     def destroy
         @outfit_item = OutfitItem.find_by(outfit_id: params[:outfit], item_id: params[:item])
         @outfit_item.delete
